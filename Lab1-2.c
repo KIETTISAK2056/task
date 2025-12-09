29458413e622d701af48b09943cabd6ee9a9c225
KIETTISAK2056 <s6804062612056@email.kmutnb.ac.th>
2025-12-09 13:45:27 +0700

    Add files via upload

---
+++ Lab1-2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+	int a,b,c;
+	scanf("%d",&a);
+	printf("%f\n",a*0.95);
+	scanf("%d",&b);
+	printf("%f\n",(a+b)*0.85);
+	scanf("%d",&c);
+	printf("%f",(a+b+c)*0.70);
+}
